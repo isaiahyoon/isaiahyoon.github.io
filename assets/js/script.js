0dd763a7eab60a0404f495e0c02ba1ff252629cb
Isaiah Yoon <isaiahyoon@berkeley.edu>
2014-11-28 15:40:45 -0800

    Remove unnecessary comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,9 @@
 $(document).ready(function(){
 	
-	// Question 4
-	// IMPLEMENT "SHOW MODAL" WHEN "CLICK ON LOGIN BUTTON FROM MAIN PAGE" HERE
 	$("#contact").click(function() {
 		$("#modal-container").fadeIn(500);
 	});
 
-	// IMPLEMENT "HIDE MODAL" WHEN "CLICK ON LOGIN BUTTON FROM MODAL BOX" HERE
 	$("#cancel").click(function() {
 		$("#modal-container").fadeOut(500);
 	});
@@ -15,12 +12,8 @@ $(document).ready(function(){
 		$("#modal-container").fadeOut(500);
 	});
 
-	// IMPLEMENT "HIDE MODAL" WHEN "CLICK ON CANCEL BUTTON FROM MODAL BOX" HERE
 	$("#modal-overlay").click(function() {
 		$("#modal-container").fadeOut(500);
 	});
-	
-	// Question 5
-	// IMPLEMENT "HIDE MODAL" WHEN "CLICK ON MODAL OVERLAY" HERE
 	
 });
