b6a85d0273d14af8c94b039c3410886150287767
Isaiah Yoon <isaiahyoon@berkeley.edu>
2017-08-10 00:19:01 -0700

    Fix js bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,19 @@
 $(document).ready(function(){
   $("#nav").load("nav.html");
   $("#modal-container").load("modal.html");
-});
-
-$(window).load(function() {
-  alert("(window).load was called - window is loaded!");
-  $("#contact").click(function() {
-    $("#modal-container").fadeIn(500);
-  });
-
-  $("#cancel").click(function() {
-    $("#modal-container").fadeOut(500);
-  });
-
-  $("#modal-overlay").click(function() {
-    $("#modal-container").fadeOut(500);
-  });
 
   $(document).keyup(function(e) {
     if (e.keyCode == 27) {
       // ESC
-      $("#modal-container").fadeOut(500);
+      closeModal();
     }
   });
 });
+
+function loadModal() {
+  $("#modal-container").fadeIn(500);
+}
+
+function closeModal() {
+  $("#modal-container").fadeOut(500);
+}
